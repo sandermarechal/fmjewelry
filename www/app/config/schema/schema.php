fc8533cf9bfb9a26dcddf474013ecfccd6679892
Sander Marechal <s.marechal@jejik.com>
2010-10-14 17:51:31 +0000

    Updated schema with the addresses table

--- www/app/config/schema/schema.php
+++ www/app/config/schema/schema.php
@@ -1,6 +1,6 @@
 <?php 
 /* SVN FILE: $Id$ */
-/* App schema generated on: 2010-08-21 12:08:26 : 1282388186*/
+/* App schema generated on: 2010-10-14 19:10:09 : 1287078669*/
 class AppSchema extends CakeSchema {
 	var $name = 'App';
 
@@ -11,6 +11,23 @@ class AppSchema extends CakeSchema {
 	function after($event = array()) {
 	}
 
+	var $addresses = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
+		'user_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'index'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'address_1' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'address_2' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'city' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'state' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'postal_code' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'country' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'primary' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+		'deleted' => array('type' => 'boolean', 'null' => false, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'user_id' => array('column' => 'user_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
 	var $groups = array(
 		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40),
